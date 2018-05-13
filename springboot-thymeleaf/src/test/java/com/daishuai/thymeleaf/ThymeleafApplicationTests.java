c7e4ab27eda68de1c41df1bf52ad7ac246923ef6
daishuai <867390427@qq.com>
2018-05-13 21:57:22 +0800

    2018-03-28

--- springboot-thymeleaf/src/test/java/com/daishuai/thymeleaf/ThymeleafApplicationTests.java
+++ springboot-thymeleaf/src/test/java/com/daishuai/thymeleaf/ThymeleafApplicationTests.java
@@ -1,4 +1,4 @@
-package com.daishuai.springbootthymeleaf;
+package com.daishuai.thymeleaf;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringbootThymeleafApplicationTests {
+public class ThymeleafApplicationTests {
 
 	@Test
 	public void contextLoads() {
